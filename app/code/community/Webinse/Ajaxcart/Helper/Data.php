fe1e800fe26ae3563a47ac8723293712b1575863
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2024-07-12 22:37:22 -0300

    Create Data.php

---
+++ app/code/community/Webinse/Ajaxcart/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Webinse_Webinse_Ajaxcart_Helper_Data
+ * PHP Version 5.5.9
+ *
+ * @category  Webinse
+ * @package   Webinse_Ajaxcart
+ * @author    Webinse Team <info@webinse.com>
+ * @copyright 2015 Webinse Ltd. (https://www.webinse.com)
+ * @license   The Open Software License 3.0
+ * @link      http://opensource.org/licenses/OSL-3.0
+ */
+
+/**
+ * Webinse_Webinse_Ajaxcart_Helper_Data
+ * Ajaxcart index controller
+ *
+ * @category  Webinse
+ * @package   Webinse_Ajaxcart
+ * @author    Webinse Team <info@webinse.com>
+ * @copyright 2015 Webinse Ltd. (https://www.webinse.com)
+ * @license   The Open Software License 3.0
+ * @link      http://opensource.org/licenses/OSL-3.0
+ */
+class Webinse_Ajaxcart_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
